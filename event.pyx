82059b631d28eaedc8e5ddeb766a3808338b969c
dugsong <dugsong@d4201f19-f223-0410-88fd-470c53b51bb3>
2004-07-15 05:25:00 +0000

    Initial revision

---
+++ event.pyx
@@ -0,0 +1,173 @@
+#
+# event.pyx
+#
+# libevent Python bindings
+#
+# Copyright (c) 2004 Dug Song <dugsong@monkey.org>
+# Copyright (c) 2003 Martin Murray <murrayma@citi.umich.edu>
+#
+# $Id$
+
+"""event library
+
+This module provides a mechanism to execute a function when a
+specific event on a file handle, file descriptor, or signal occurs,
+or after a given time has passed.
+"""
+
+import sys
+
+cdef extern from "Python.h":
+    void Py_INCREF(object o)
+    void Py_DECREF(object o)
+
+cdef extern from "event.h":
+    struct timeval:
+        unsigned int tv_sec
+        unsigned int tv_usec
+    
+    struct event_t "event":
+        int   ev_fd
+        int   ev_flags
+        void *ev_arg
+
+    void event_init()
+    void event_set(event_t *ev, int fd, short event,
+                   void(*)(int, short, void *), void *arg)
+    void evtimer_set(event_t *ev, void (*)(int, short, void *), void *arg)
+    int  event_add(event_t *ev, timeval *tv)
+    int  event_del(event_t *ev)
+    int  event_dispatch()
+    int  event_loop(int loop)
+    int  event_pending(event_t *ev, short, timeval *tv)
+
+    int EVLOOP_ONCE
+    int EVLOOP_NONBLOCK
+
+EV_TIMEOUT = 0x01
+EV_READ    = 0x02
+EV_WRITE   = 0x04
+EV_SIGNAL  = 0x08
+EV_PERSIST = 0x10
+
+__event_exc = None
+
+cdef int __event_inited
+
+cdef int __event_sigcb():
+    return -1
+
+cdef void __event_handler(int fd, short evtype, void *arg):
+    (<object>arg).__callback(evtype)
+
+cdef class event:
+    """event(callback, arg=None, evtype=0, handle=None) -> event object
+
+    Create a new event object with a user callback.
+
+    Arguments:
+
+    callback -- user callback with (event, handle, evtype, arg) prototype
+    arg      -- optional callback argument
+    evtype   -- bitmask of EV_READ or EV_WRITE, or EV_SIGNAL
+                XXX - for EV_SIGNAL, EV_PERSIST is implicit
+    handle   -- for EV_READ or EV_WRITE, a file handle or descriptor
+                for EV_SIGNAL, a signal number
+    """
+    cdef event_t ev
+    cdef object callback, arg, handle
+
+    def __init__(self, callback, arg=None, short evtype=0, handle=-1):
+        self.callback = callback
+        self.arg = arg
+        self.handle = handle
+        if evtype == 0 and not handle:
+            evtimer_set(&self.ev, __event_handler, <void *>self)
+        elif evtype == EV_SIGNAL:
+            evtype = evtype | EV_PERSIST
+            event_set(&self.ev, handle, evtype, __event_handler, <void *>self)
+        else:
+            if type(handle) != int:
+                handle = handle.fileno()
+            event_set(&self.ev, handle, evtype, __event_handler, <void *>self)
+
+    def __callback(self, evtype):
+        cdef extern int event_gotsig
+        cdef extern int (*event_sigcb)()
+        global __event_exc
+        try:
+            self.callback(self, self.handle, evtype, self.arg)
+            if not self.pending():
+                Py_DECREF(self)
+        except:
+            event_sigcb = __event_sigcb
+            event_gotsig = 1
+            __event_exc = sys.exc_info()
+    
+    def add(self, double timeout=0.0):
+        """Add event to be executed after an optional timeout.
+
+        Arguments:
+        
+        timeout -- seconds after which the event will be executed
+        """
+        cdef timeval tv
+
+        if not self.pending():
+            Py_INCREF(self)
+        if timeout > 0.0:
+            tv.tv_sec = <long>timeout
+            tv.tv_usec = (timeout - <double>timeout) * 1000000.0
+            event_add(&self.ev, &tv)
+        else:
+            event_add(&self.ev, NULL)
+
+    def pending(self):
+        """Return 1 if the event is scheduled to run, or else 0."""
+        return event_pending(&self.ev, EV_TIMEOUT|EV_SIGNAL|EV_READ|EV_WRITE, NULL)
+    
+    def delete(self):
+        """Remove event from the event queue."""
+        if self.pending():
+            event_del(&self.ev)
+            Py_DECREF(self)
+    
+    def __dealloc__(self):
+        self.delete()
+    
+    def __repr__(self):
+        return '<event flags=0x%x, handle=%s, callback=%s, arg=%s>' % \
+               (self.ev.ev_flags, self.handle, self.callback, self.arg)
+
+def init():
+    """Initialize event queue."""
+    if not __event_inited:
+        event_init()
+        __event_inited = 1
+
+def add(event ev, double timeout=0):
+    """Add the specified event to the event queue, with an optional timeout."""
+    ev.add(timeout)
+
+def delete(event ev):
+    """Delete the specified event."""
+    ev.delete()
+
+def dispatch():
+    """Dispatch all events on the event queue."""
+    global __event_exc
+    event_dispatch()
+    if __event_exc:
+        raise __event_exc[0], __event_exc[1], __event_exc[2]
+
+def loop(nonblock=False):
+    """Dispatch all pending events on queue in a single pass."""
+    cdef int flags
+    flags = EVLOOP_ONCE
+    if nonblock:
+        flags = EVLOOP_ONCE|EVLOOP_NONBLOCK
+    event_loop(flags)
+
+if not __event_inited:
+    # XXX - make sure event queue is always initialized.
+    event_init()
